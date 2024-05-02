361138c4ef35291578d66d5298ae28a00ba1ff27
Ander Cabrera <andercabrerac@gmail.com>
2024-05-02 16:29:07 -0600

    refs practice

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useRef, useState } from "react";
+
 export const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputvalue, setInputValue] = useState('');
+  const node = useRef(null);
+
+  // console.log(node.current);
+
+  useEffect(() => {
+    
+    console.log(inputvalue);
+  }, []);
+  
   return (
     <div>
       <h1>Task List</h1>
+      <input type="text" ref={node} onChange={(e) => {
+        setInputValue(e.target.value);
+      }}/>
     </div>
   );
 };
