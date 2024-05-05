8d70eb164370936004338861c77bc2e1f192ce22
Ander Cabrera <andercabrerac@gmail.com>
2024-05-05 12:33:22 -0600

    basic table done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,36 +1,72 @@
-import '../output.css'
+/* eslint-disable react/prop-types */
+import '../output.css';
 
-export const Task = ({ tasks }) => {
+export const Task = ({ tasks, updateHandler, deleteHandler }) => {
   return (
     <>
-      <div className="container">
-        <div className="flex justify-center items-center h-screen">
-          <div className="w-full max-w-md">
-            <div className="bg-[#555] shadow-lg rounded px-8 pt-6 pb-8 mb-4">
-              {tasks.map((task) => (
-                <div key={task._id} className="mb-4">
-                  <div className="text-white text-sm font-bold mb-2">
-                    {task.task_name}
-                  </div>
-                  <div className="text-white text-sm font-bold mb-2">
-                    {task.task_description}
-                  </div>
-                  <div className="text-white text-sm font-bold mb-2">
-                    {task.date_start.slice(0, 10) + ' ' + task.date_start.slice(11, 19)}
-                  </div>
-                  <div className="text-white text-sm font-bold mb-2">
-                    {task.date_end}
-                  </div>
-                  <div className="text-white text-sm font-bold mb-2">
-                    {task.status ? 'Completed' : 'Pending'}
-                  </div>
-
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <table className="table-auto w-[80%] justify-center">
+        <thead className="">
+          <tr className="bg-slate-900">
+            <th className="border border-solid border-white text-white">
+              Task Name
+            </th>
+            <th className="border border-solid border-white text-white">
+              Task Description
+            </th>
+            <th className="border border-solid border-white text-white">
+              Date Start
+            </th>
+            <th className="border border-solid border-white text-white">
+              Date End
+            </th>
+            <th className="border border-solid border-white text-white">
+              Task Status
+            </th>
+            <th className="border border-solid border-white text-white">
+              User
+            </th>
+            <th className="border border-solid border-white text-white">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task, index) => (
+            <tr
+              key={index}
+              className="even:bg-gray-200 odd:bg-white text-center"
+              id={task._id}
+            >
+              <td>{task.task_name}</td>
+              <td>{task.task_description}</td>
+              <td>{new Date(task.date_start).toLocaleString()}</td>
+              <td>{new Date(task.date_end).toLocaleString()}</td>
+              <td>
+                {task.task_status ? (
+                  <span className="text-green-600">Completed</span>
+                ) : (
+                  <span className="text-red-600">Incomplete</span>
+                )}
+              </td>
+              <td>{task.user.name}</td>
+              <td>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-3 rounded m-1"
+                  onClick={updateHandler}
+                >
+                  <i className="fa-solid fa-pencil"></i>
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1.5 px-3 rounded m-1"
+                  onClick={deleteHandler}
+                >
+                  <i className="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,25 +1,23 @@
-import { useEffect, useRef, useState } from "react";
-import { Task } from "../components/Task.jsx";
-import { getTasks } from "../services/task.service.js";
+import { useEffect, useState } from 'react';
+import { Task } from '../components/Task.jsx';
+import { getTasks } from '../services/task.service.js';
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState([]);
   const [inputvalue, setInputValue] = useState('');
-  const node = useRef(null);
 
   useEffect(() => {
-    getTasks()
-      .then((data) => {
-        setTasks(data);
-      });
+    getTasks().then((data) => {
+      setTasks(data);
+    });
   }, [inputvalue]);
 
-  
-  
   return (
-    <div>
+    <div className="">
       <h1>Task List</h1>
-      <Task tasks={tasks} />
+      <div className="tasks-container w-full flex flex-col items-center">
+        <Task tasks={tasks} />
+      </div>
     </div>
   );
 };
